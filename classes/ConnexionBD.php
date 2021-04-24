e14460319496a6c8f8e63c682b10346bd6787166
Farid-Adwani <78969623+Farid-Adwani@users.noreply.github.com>
2021-04-24 01:54:37 +0100

    upload farid version

--- classes/ConnexionBD.php
+++ classes/ConnexionBD.php
@@ -2,16 +2,16 @@
 
 class ConnexionBD
 {
-    private static $_dbname = "firstdb";
-    private static $_user = "root";
-    private static $_pwd = "";
-    private static $_host = "localhost";
+    private static $_dbname = "sql11407829";
+    private static $_user = "sql11407829";
+    private static $_pwd = "mnTjGN13fB";
+    private static $_host = "sql11.freesqldatabase.com";
     private static $_bdd = null;
 
     private function __construct()
     {
         try {
-            self::$_bdd = new PDO("mysql:host=".self::$_host.";dbname=".self::$_dbname.";charset=utf8",self::$_user, self::$_pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
+            self::$_bdd = new PDO("mysql:host=".self::$_host.";dbname=".self::$_dbname.";port=3306".";charset=utf8",self::$_user, self::$_pwd,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
         } catch (PDOException $e) {
             echo "uhui";
             die('Erreur : ' . $e->getMessage());
